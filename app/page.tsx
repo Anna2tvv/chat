ce6b96bce26bbdb6a2c74a72fa7d23b6e815b156
Anna O <2tvv21@gmail.com>
2024-02-12 22:40:11 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+  const modal = useRef<HTMLDialogElement>(null);
+  useEffect(() => {
+    modal.current?.showModal();
+  });
+  return (
+    <main>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <dialog ref={modal} className="modal">
+        <div className="modal-box flex flex-col justify-center items-center gap-4">
+          <h3 className="font-bold text-lg">What's your name?</h3>
+          <input
+            type="text"
+            placeholder="Name.."
+            className="input input-bordered input-primary border-black w-full max-w-xs"
+          />
+          <div>
+            <button className="btn btn-outline btn-success">Let's Start</button>
+          </div>
+        </div>
+      </dialog>
+    </main>
+  );
+}
