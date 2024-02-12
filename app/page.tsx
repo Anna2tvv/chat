667d04e39948c5e227879927d85aaaea5db383f4
Anna O <2tvv21@gmail.com>
2024-02-12 23:04:58 +0200

    added user name

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const modal = useRef<HTMLDialogElement>(null);
   useEffect(() => {
     modal.current?.showModal();
   });
+  const [username, setUsername] = useState("");
   return (
     <main>
       <dialog ref={modal} className="modal">
         <div className="modal-box flex flex-col justify-center items-center gap-4">
           <h3 className="font-bold text-lg">What's your name?</h3>
           <input
+            value={username}
+            onChange={(value) => setUsername(value.target.value)}
             type="text"
             placeholder="Name.."
             className="input input-bordered input-primary border-black w-full max-w-xs"
