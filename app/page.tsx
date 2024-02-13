b19e6372767e9689e35b1b8773e6498e4c75e5a3
Anna O <2tvv21@gmail.com>
2024-02-13 23:50:59 +0200

    feat: add socket io, complete username

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { io } from "socket.io-client";
 
 export default function Home() {
   const modal = useRef<HTMLDialogElement>(null);
+  const socket = io("http://localhost:5000");
   useEffect(() => {
     modal.current?.showModal();
+    return () => {
+      socket.close();
+    };
   });
   const [username, setUsername] = useState("");
+  const [isStarted, setIsStarted] = useState(false);
+  const [error, setError] = useState<string>();
+  function handleStart() {
+    if (username.length > 0) {
+      setIsStarted(true);
+      modal.current?.close();
+      return;
+    }
+    setError("username must be greater than 0");
+  }
+  function handleChangeOfUsername(value: ChangeEvent<HTMLInputElement>) {
+    setUsername(value.target.value);
+    setError(undefined);
+  }
   return (
     <main>
       <dialog ref={modal} className="modal">
@@ -15,13 +40,19 @@ export default function Home() {
           <h3 className="font-bold text-lg">What's your name?</h3>
           <input
             value={username}
-            onChange={(value) => setUsername(value.target.value)}
+            onChange={handleChangeOfUsername}
             type="text"
             placeholder="Name.."
             className="input input-bordered input-primary border-black w-full max-w-xs"
           />
+          {error && <p>{error}</p>}
           <div>
-            <button className="btn btn-outline btn-success">Let's Start</button>
+            <button
+              onClick={handleStart}
+              className="btn btn-outline btn-success"
+            >
+              Let's Start
+            </button>
           </div>
         </div>
       </dialog>
